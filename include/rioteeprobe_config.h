87cc7c82c2410101633ce14b7ba13ea8a78fa758
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-04-21 16:58:03 +0200

    adjust pin config to latest board/probe design

--- include/rioteeprobe_config.h
+++ include/rioteeprobe_config.h
@@ -15,7 +15,7 @@
 #define PROBE_PIN_SBWIO 19
 
 /* Controls direction of level translators */
-#define PROBE_PIN_PROG_DIR 15
+#define PROBE_PIN_PROG_DIR 14
 
 /* Controls target fixed power supply */
 #define PROBE_PIN_TARGET_POWER 0
@@ -43,7 +43,7 @@
 #define PROBE_USB_VID 0x1209
 #define PROBE_USB_PID 0xC8A1
 
-#define PROBE_PIN_LED 2
+#define PROBE_PIN_LED 1
 
 #define PROBE_PIN_SWDCLK 18
 #define PROBE_PIN_SWDIO 16
@@ -55,16 +55,16 @@
 #define PROBE_PIN_PROG_DIR 15
 
 /* Controls target fixed power supply */
-#define PROBE_PIN_TARGET_POWER 3
+#define PROBE_PIN_TARGET_POWER 2
 
 /* Controls power for programming level translators */
 #define PROBE_PIN_TRANS_PROG_EN 20
 
 /* Controls power for UART TX level translator */
-#define PROBE_PIN_TRANS_UARTTX_EN 5
+#define PROBE_PIN_TRANS_UARTTX_EN 11
 
 /* Controls power for UART RX level translator */
-#define PROBE_PIN_TRANS_UARTRX_EN 6
+#define PROBE_PIN_TRANS_UARTRX_EN 12
 
 /* Uart TX to target */
 #define PROBE_UART_TX 8
