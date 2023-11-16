f3103f68381838400c433cbc6db067fff716c47d
Ingmar Splitt <orgua@users.noreply.github.com>
2023-11-16 11:37:22 +0100

    add initial testsuite

---
+++ tool/tests/test_cli.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+from riotee_probe.cli import cli
+
+
+def test_cli_list_probes(
+    cli_runner: CliRunner,
+) -> None:
+    res = cli_runner.invoke(cli, ["list"])
+    assert res.exit_code == 0
+
+
+def test_cli_print_version(
+    cli_runner: CliRunner,
+) -> None:
+    res = cli_runner.invoke(cli, ["--version", "list"])
+    assert res.exit_code == 0
